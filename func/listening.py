6474ddca3b3922297a2caea55a782cc34e0641ac
EMMMABK <adilbikiev2@gmail.com>
2024-11-03 18:45:01 +0600

    add 3rd commit

--- func/listening.py
+++ func/listening.py
@@ -2,6 +2,15 @@ import speech_recognition as sr
 import tkinter as tk
 from threading import Thread
 
+# Функция для загрузки слов из файла
+def load_words(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [line.strip().lower() for line in file.readlines()]
+
+# Загрузка мусульманских слов из файла
+all_words = load_words('words.txt')
+count = 0
+
 # Функция для распознавания речи
 def listen():
     recognizer = sr.Recognizer()
@@ -11,25 +20,30 @@ def listen():
                 print("Слушаю...")
                 audio = recognizer.listen(source)
                 text = recognizer.recognize_google(audio, language='ru-RU')
-                if "альхамдулилях" in text.lower():
-                    update_count()
+                check_words(text.lower())
             except sr.UnknownValueError:
                 continue
             except sr.RequestError:
                 print("Ошибка сервиса распознавания.")
                 break
 
+# Функция для проверки слов и обновления счётчика
+def check_words(text):
+    global count
+    for word in all_words:
+        if word in text:
+            count += 1
+            update_count()
+            break  # Убедитесь, что каждое слово считается только один раз за одно произнесение
+
 # Функция для обновления счётчика
 def update_count():
-    global count
-    count += 1
     count_label.config(text=f"Количество: {count}")
 
 # Создание графического интерфейса
 root = tk.Tk()
-root.title("Счетчик Альхамдулилях")
+root.title("Счетчик Мусульманских Слов")
 
-count = 0
 count_label = tk.Label(root, text=f"Количество: {count}", font=("Helvetica", 24))
 count_label.pack(pady=20)
 
